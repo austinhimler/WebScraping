7a810a4880b416a379777bf90b94a2f5c69e74ac
austinhimler <usclaxx10@gmail.com>
2023-11-29 19:04:14 -0500

    Add project files.

---
+++ WebScraping/Program.cs
@@ -0,0 +1,18 @@
+﻿using HtmlAgilityPack;
+
+namespace WebScraping
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string url = "https://en.wikipedia.org/wiki/List_of_SpongeBob_SquarePants_episodes";
+            var web = new HtmlWeb();
+            var document = web.Load(url);
+
+            var nodes = document.DocumentNode.SelectNodes("//*[@id=\"mw-content-text\"]/div[1]/table[position()>1 and positon()<15]/tbody/tr[position()>1]");
+
+
+        }
+    }
+}
